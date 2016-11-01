63440aa74d74a3a6904fd9ca4704c47aba481872
Chawasit Tengtrairatana <chawasit_teng@cmu.ac.th>
2016-11-01 17:03:53 +0700

    init
    init libgdx project

---
+++ desktop/src/com/zalzer/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.zalzer.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zalzer.game.PruetDefender;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = PruetDefender.WIDTH;
+        config.height = PruetDefender.HEIGHT;
+        config.title = PruetDefender.TITLE;
+		new LwjglApplication(new PruetDefender(), config);
+	}
+}
